313187289211931534b574894642b1da688a08ff
akuisara <akuisara@gmail.com>
2014-10-07 15:36:31 -0400

    added a function to populate list

--- hw12/linked_list.js
+++ hw12/linked_list.js
@@ -1,52 +1,70 @@
 console.log("Linked List");
 
-function Node() {
-	state = null;
-	abbr = null;
-	next = null;
+function Node(string1,string2) {
+	this.state = string1;
+	this.abbreviation = string2;
+	this.next = null;
 }
 
-function LinkedList() {
-	head = null;
-	tail = null;
-	size = 0;
-}
+var LinkedList = function() {
+	this.head = null;
+	this.tail = null;
+	this.num = 0;
+};
 
-function Populate(state, abbr) {
-	var newNode = new Node();
-	newNode.state = state;
-	newNode.abbr = abbr;
-	tail = newNode;
+LinkedList.prototype.Populate = function() {
+	var fs = require('fs');
 
-	if (head === null) {
-		head = newNode;
-	}else {
-		head.next = newNode;
-	}
-	size++;
-}
+	fs.readFile("states.csv", {encoding: 'utf-8'}, function(err,data){
+		data=data.split(/\n+/);
+		var stateName = [], abbreviationName = [];
+
+		for (var i = 0; i < data.length - 1; i++) {
+			var string1 = "";
+			for (var j = 0; j < data[i].length - 5; j++) {
+				string1+= data[i][j];
+			}
+			stateName[i] = string1;
+			var string2 = "";
+			for (var k = (data[i].length - 4); k < data[i].length; k++) {
+				string2+= data[i][k];
+			}
+			abbreviationName[i] = string2;
+
+			var newNode = new Node(string1,string2);
+
+			if (this.head === null){
+				this.head = newNode;
+				this.tail = newNode;
+			}
+			else {
+				this.head.next = newNode;
+				this.tail = newNode;
+			}
+
+			this.num++;
+
+			if (err){
+				console.log(err);
+			}
+		}
+	});
+};
+
+
+LinkedList.prototype.DisplayContent = function() {
+	var fs = require('fs');
+	var outputFile = fs.open("Output.txt", 3);
+	
+};
 
 function Driver(){
 	var list = new LinkedList();
 
-	var fs = require('fs');
-	var path = require('path');
+	list.Populate();
 
-	fs.readFile("states.csv", {encoding: 'utf-8'}, function(err,data){
-		data=data.split(/\n+/);
-		var stateName = [], abbrName = [];
+	list.DisplayContent();
 
-		for (var i = data.length - 1; i >= 0; i--) {
-			stateName = data[i].split(/\"\,\"+/);
-		}
-
-		if (!err){
-			console.log(stateName);
-		}else{
-			console.log(err);
-		}
-
-	});
 }
 
 Driver();
